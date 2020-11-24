f136d30d98498f03074871c5192244308ff8004b
Kishor <kishor1124@gmail.com>
2020-11-24 14:27:50 +0530

    added setter injection

--- spring-core-demo/src/main/java/com/org/JpaImpl.java
+++ spring-core-demo/src/main/java/com/org/JpaImpl.java
@@ -2,9 +2,17 @@ package com.org;
 
 public class JpaImpl implements EmployeeDao {
 
+	
+	private DBConfig dbConfig ;
+	
 	@Override
 	public void store() {
-		System.out.println("store() implementation done via JPA");
+		System.out.println("store() implementation done via JPA ");
+		System.out.println("DB Config: Url = "+dbConfig.getDbUrl()+", Name = "+dbConfig.getDbname());
+	}
+
+	public void setDbConfig(DBConfig dbConfig) {
+		this.dbConfig = dbConfig;
 	}
 
 }
